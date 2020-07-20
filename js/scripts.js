5686e9782de1a6f63fd9c3ca9a77c32508c3563e
Ondicho <ondicho53@gmail.com>
2020-07-20 21:01:04 +0300

    this commit adds a getvalue() function that gets the user input through a form

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
-var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-//day of the week
-function checkResult(){
-  let day=document.getElementsById('day').value
-  let month=document.getElementsById('month').value
-  let year=document.getElementsById('year').value
-}
-var CC=parseInt("year".slice(0,1));
-var YY=parseInt("year".slice(2,3));
-var MM=parseInt("month");
-var DD=parseInt("day");
-
-var dayOfTheWeek=function (CC,YY,MM,DD) {
-  return (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
-}
+function getValue() {
+  var date = document.getElementById("mydate").value;
+  alert(date);
+    }
