ad05389b281516bfc195f32ea719c6b962906840
Ondicho <ondicho53@gmail.com>
2020-07-20 23:40:44 +0300

    thhis commit adds radio buttins to the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//this function accepts the date and returns it as output in the results tab
 function getValue() {
   var date = document.getElementById("mydate").value;
   document.getElementById("resultsBody").innerHTML=date;
