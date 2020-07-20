981d535159eaf64c02603790b88e1dbdba48fec3
Ondicho <ondicho53@gmail.com>
2020-07-20 22:52:15 +0300

    This commit causes the results tab to disappear and appear only when input has been provided

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 function getValue() {
   var date = document.getElementById("mydate").value;
-  alert(date);
-    }
+  document.getElementById("resultsBody").innerHTML=date;
+$("#results").show();
+$("#resultsB").show();
+};
