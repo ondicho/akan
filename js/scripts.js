48efca33bf2ab6c8d86eee51c234873a7e614e05
Ondicho <ondicho53@gmail.com>
2020-07-21 09:01:53 +0300

    this commit adds a feature for calculating day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,8 @@ var CC=yearString.slice(0,2);
 var YY=yearString.slice(2,4);
 var MM=birthDate.getMonth()+1;
 var DD=birthDate.getDate();
+var d=(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+var dayOfTheWeek=d.toFixed(0);
 
-document.getElementById("fourth").innerHTML=YY;
+document.getElementById("fourth").innerHTML=dayOfTheWeek;
 }
