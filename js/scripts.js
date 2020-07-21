cc047c95a62769a54da2dc942d7f24eed02de2b0
Ondicho <ondicho53@gmail.com>
2020-07-21 07:38:48 +0300

    this commit addds a functionality for accepting the gender of the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-//this function accepts the date and returns it as output in the results tab
+
 function getValue() {
+  event.preventDefault()
   var date = document.getElementById("mydate").value;
-  document.getElementById("resultsBody").innerHTML=date;
-$("#results").show();
-$("#resultsB").show();
-};
+  var x=document.getElementById("male").checked;
+  var y=document.getElementById("female").checked;
+
+  document.getElementById("first").innerHTML=date;
+  $("#results").show();
+  $("#resultsBody").show()
+
+
+var year=parseInt(date);
+document.getElementById("second").innerHTML=year;
+
+if (x=1) {
+  document.getElementById("third").innerHTML="I am male";
+} else if (y=1) {
+  document.getElementById("third").innerHTML="I am female";
+}
+}
