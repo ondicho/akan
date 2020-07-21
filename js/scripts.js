7812f192f90377264934da6734245485e93208a5
Ondicho <ondicho53@gmail.com>
2020-07-21 10:42:43 +0300

    adds control flows

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,10 @@ function getValue() {
   $("#resultsBody").show()
 
 
-var year=parseInt(date);
-document.getElementById("second").innerHTML=year;
-
-if (x===1 && x!==0) {
-  document.getElementById("third").innerHTML="I am male";
-} else {
-  document.getElementById("third").innerHTML="I am female";
+if (x===true) {
+  document.getElementById("second").innerHTML="Gender: male";
+} else{
+  document.getElementById("second").innerHTML="Gender: female";
 }
 
 var birthDate=new Date(date);
@@ -28,6 +25,51 @@ var MM=birthDate.getMonth()+1;
 var DD=birthDate.getDate();
 var d=(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
 var dayOfTheWeek=d.toFixed(0);
+document.getElementById("day").innerHTML=dayOfTheWeek;
 
-document.getElementById("fourth").innerHTML=dayOfTheWeek;
+var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+if (dayOfTheWeek==1 && x==true) {
+  document.getElementById("fourth").innerHTML=male[0];
+}
+else if(dayOfTheWeek==1 && x!=true)  {
+  document.getElementById("fourth").innerHTML=female[0];
+}
+else if (dayOfTheWeek==2 && x==true) {
+  document.getElementById("fourth").innerHTML=male[1];
+}
+else if(dayOfTheWeek==2 && x!=true)  {
+  document.getElementById("fourth").innerHTML=female[1];
+}
+else if (dayOfTheWeek==3 && x==true) {
+  document.getElementById("fourth").innerHTML=male[2];
+}
+else if(dayOfTheWeek==3 && x!=true)  {
+  document.getElementById("fourth").innerHTML=female[2];
+}
+else if (dayOfTheWeek==4 && x==true) {
+  document.getElementById("fourth").innerHTML=male[3];
+}
+else if(dayOfTheWeek==4 && x!=true)  {
+  document.getElementById("fourth").innerHTML=female[3];
+}
+else if (dayOfTheWeek==5 && x==true) {
+  document.getElementById("fourth").innerHTML=male[4];
+}
+else if(dayOfTheWeek==5 && x!=true)  {
+  document.getElementById("fourth").innerHTML=female[4];
+}
+else if (dayOfTheWeek==6 && x==true) {
+  document.getElementById("fourth").innerHTML=male[5];
+}
+else if(dayOfTheWeek==6 && x!=true)  {
+  document.getElementById("fourth").innerHTML=female[5];
+}
+else if (dayOfTheWeek==7 && x==true) {
+  document.getElementById("fourth").innerHTML=male[6];
+}
+else {
+  document.getElementById("fourth").innerHTML=female[6];
+}
 }
